232d14041fa7f0d94addc1e1d3d53728b3a762d1
Derick Hansraj <derickhansraj@ymail.com>
2021-10-16 10:38:03 -0400

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,27 +1,42 @@
 import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
 import { CAMPSITES } from "../shared/campsites";
+import CampSiteInfo from "./CampsiteInfoComponent";
 import Home from "./HomeComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Contact from "./ContactComponent";
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from "../shared/comments";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        campsites: CAMPSITES,
-        comments: COMMENTS,
-        partners: PARTNERS,
-        promotions: PROMOTIONS
+      campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS,
     };
-}
+  }
 
   render() {
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampSiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
     const HomePage = () => {
       return <Home />;
     };
@@ -30,6 +45,7 @@ class Main extends Component {
       <div>
         <Header />
         <Switch>
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Route path="/home" component={HomePage} />
           <Route
